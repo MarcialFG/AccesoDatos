a1f4bcbe1827fdac6271b2b81b70f939a402da37
Marcial <Marcial@DESKTOP-CU0UBAB>
2021-10-05 09:02:10 +0200

    Primera Subida

--- SalasChatMarPa/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.Marcial.SalasChatMarPa {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.Marcial.SalasChatMarPa to javafx.fxml;
-    exports com.Marcial.SalasChatMarPa;
-}
